43d706d1a76c2a739c17521416ff70ba89482ec3
D <jossedaviid1@gmail.com>
2023-04-25 15:39:21 -0500

    add: initial page

--- src/pages/validate.tsx
+++ src/pages/validate.tsx
@@ -1,13 +1,36 @@
+import Image from 'next/image'
 import { Layout } from '@/components/Layout';
-import styles from '@/styles/Home.module.css';
+import DogValidate from '@/assets/dog-validate.jpg';
+import styles from '@/styles/Validate.module.scss';
 
 
 export default function Validate() {
   return (
     <Layout>
-      <main className={styles.main}>
-        <div className={styles.center}>
-          <h1>Validate</h1>
+      <main className={styles.validate}>
+        <h2>Registration Verification</h2>
+        <h4>Verify Animal Registration</h4>
+        <div className={styles.container}>
+          <div className={styles.information}>
+            <Image
+              src={DogValidate}
+              alt="Picture of the author"
+              width={300}
+              height={200}
+            />
+            <p>
+              Services Dog School is the nation's largest Support Animal registry.
+              Enter your registration number (found on your animal's ID, begins with either ES- or SA-)
+              below to verify your animal's registration information.
+              Please click on the next tab to verify your ESA Letter.
+              Any questions? Email us at <a href="mailto:support@servicesdogschool.com">support@servicesdogschool.com</a>
+            </p>
+          </div>
+          <div className={styles.inputContainer}>
+            <span>Verify Registration</span>
+            <input type="text" placeholder='Enter your ID' />
+            <button>Verify</button>
+          </div>
         </div>
       </main>
     </Layout>
