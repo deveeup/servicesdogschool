6c15eab8802f53f2f35086ccdb9aa9b06c501331
D <jossedaviid1@gmail.com>
2023-04-25 15:08:18 -0500

    add: colors and pages

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,17 @@
 export enum URL {
   home = '/',
   information = '/information',
-  register = '/register',
+  shop = '/shop',
   registry = '/registry',
   validate = '/validate',
+  signIn = '/sign-in'
+};
+
+export enum TITLES {
+  home = "Home",
+  information = "Information",
+  shop = "Shop",
+  registry = "Registry",
+  validate = "Validate",
+  signIn = "Sign In",
 };
--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -2,12 +2,12 @@ import { Layout } from '@/components/Layout'
 import styles from '@/styles/Home.module.css'
 
 
-export default function Register() {
+export default function Shop() {
   return (
     <Layout>
       <main className={styles.main}>
         <div className={styles.center}>
-          <h1>Register</h1>
+          <h1>Shop</h1>
         </div>
       </main>
     </Layout>
